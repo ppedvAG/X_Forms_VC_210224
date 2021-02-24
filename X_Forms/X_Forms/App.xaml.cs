deacb46261ff3477a4a458ba33a006c0ce375d42
KlaasHagge <klaash@ppedv.de>
2021-02-24 14:38:26 +0100

    Add project files.

---
+++ X_Forms/X_Forms/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace X_Forms
+{
+    public partial class App : Application
+    {
+        //Die App-Klasse beinhaltet eine grundlegen Initialisierung der App sowie die MainPage-Property, welche defniert,
+        //welche Page gerade in der App angezeigt wird. Diese Property wird auch als Einstiegspunkt verwendet.
+        public App()
+        {
+            InitializeComponent();
+
+            //Zuweisung der MainPage-Property zu einer Page
+            MainPage = new MainPage();
+        }
+
+        public DateTime TimeStamp { get; set; }
+
+        //Methoden, welche zu bestimmten globalen Events ausgeführt werden (Start, Unterbrechen der App [Sleep], Wiederaktivierung der App [Resume])
+        protected override void OnStart()
+        {
+            MainPage.DisplayAlert("Time", $"Startzeit: {DateTime.Now}", "ok");
+        }
+
+        protected override void OnSleep()
+        {
+            TimeStamp = DateTime.Now;
+        }
+
+        protected override void OnResume()
+        {
+            MainPage.DisplayAlert("Time", $"Geschlafene Zeit: {DateTime.Now.Subtract(TimeStamp).TotalSeconds}", "ok");
+        }
+    }
+}
---
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace X_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        //Property zum Zwischenspeichern der Personenliste (ObservableCollection ist eine generische Liste, welche die GUI
+        //automatisch über Veränderungen informiert
+        public ObservableCollection<string> Namensliste { get; set; }
+
+        //Konstruktor
+        public MainPage()
+        {
+            //Initialisierung der UI (Xaml-Datei). Sollte immer erste Aktion des Konstruktors sein
+            InitializeComponent();
+
+            //Zuweisung von EventHandlern zu den Completed-Events, damit ein besserer Bedienfluss gegeben ist
+            Ent_Vorname.Completed += (s, e) => Ent_Nachname.Focus();
+            //Erstellen eines neuen Listenelements (aus UI-Properties)
+            Ent_Nachname.Completed += (s, e) => Namensliste.Add(Ent_Vorname.Text + " " + Ent_Nachname.Text);
+
+            //Initialisierung der Namesliste
+            Namensliste = new ObservableCollection<string>()
+            {
+                "Anna Nass",
+                "Rainer Zufall"
+            };
+
+            //Durch Setzen des BindingContextes nehmen Kurzbindungen aus dem XAML-Code automatisch Bezug auf die Properties
+            //des im BindingContext gesetzten Objekts
+            this.BindingContext = this;
+        }
+
+        //EventHandler
+        private void Btn_KlickMich_Clicked(object sender, EventArgs e)
+        {
+            //Neuzuweisung einer UI-Property über die x:Name-Property des Steuerelements
+            Lbl_Main.Text = "Danke, dass du auf den Button geklickt hast.";
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            //Anzeige einer 'MessageBox' und Abfrage der User-Antwort
+            bool erg = await DisplayAlert("Begrüßung", "Sei gegrüßt", "Danke", "Interessiert mich nicht");
+
+            if (erg)
+                await DisplayAlert("Gerne", "Gerne", "ok");
+        }
+    }
+}
